01343c634082beaa431e9b1f8dfd7c459d0f0e17
davinky <tmbwpva@protonmail.com>
2023-12-27 06:24:49 +0700

    Adding main menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
-import 'package:flame/game.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:misteri_prasasti/game/main_game.dart';
+import 'package:get/get.dart';
+import 'package:misteri_prasasti/main_menu/main_menu_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(GameWidget(game: MainGame()));
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Get.putAsync(() async => await SharedPreferences.getInstance());
+  runApp(MaterialApp(
+    home: MainMenuView(),
+  ));
 }
---
+++ lib/main_menu/main_menu_view.dart
@@ -0,0 +1,84 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:misteri_prasasti/game/main_game.dart';
+
+class MainMenuView extends StatelessWidget {
+  MainMenuView({super.key});
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.grey, Colors.red],
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Image(image: AssetImage('assets/images/logo_main_menu.png')),
+          Container(
+            margin: const EdgeInsets.only(top: 32),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: FilledButton(
+              onPressed: () {
+                runApp(GameWidget<FlameGame>(game: MainGame()));
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              ),
+              child: const Text(
+                'PLAY',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: FilledButton(
+              onPressed: () {
+                _googleSignIn.signIn().then((user) {
+                  if (user?.displayName != null) {
+                    runApp(GameWidget<FlameGame>(game: MainGame()));
+                    Get.snackbar(
+                        'Success',
+                        'Halo, ${user!.displayName}'
+                    );
+                  } else {
+                    Get.snackbar(
+                        'Success',
+                        'Halo, Player'
+                    );
+                  }
+                }).catchError((error) {
+                  Get.snackbar(
+                      'Failed',
+                      'Login Gagal'
+                  );
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white)
+              ),
+              child: const Text(
+                'Continue with Google',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
